a6efd1fdcc0ab3294b4fc52db5bdc9b86c7abce7
Brian Monsales <brianmonsales@gmail.com>
2025-04-01 11:52:56 +0800

    partial update on hero component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,7 @@
 <header class="flex justify-between items-center py-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 fixed w-full top-0 left-1/2 transform -translate-x-1/2 ">
-    <a href="/" class="text-2xl font-bold">Brian's Portfolio</a>
+    <a href="#home" class="text-2xl font-bold">
+        <img src="{logo.src}" alt="logo" class="w-10 h-10 inline-block" />
+    </a>
     <nav id="header-links">
         {#each anchors as anchor}
             <a href={`#${anchor}`} class="text-lg font-semibold mx-4" on:click={handleClick}>{anchor}</a>
@@ -8,14 +10,29 @@
 </header>
 
 <script>
+    import logo from '@assets/logo-white.png';
+    import { onMount } from 'svelte';
     const anchors = ['home', 'about', 'projects', 'contact'];
+    const activeClasses = ['active', 'bg-gradient-to-r', 'from-fuchsia-600', 'to-indigo-600', 'bg-clip-text', 'text-transparent'];
+
+    onMount(() => {
+        const links = document.querySelectorAll('nav#header-links a');
+        console.log(logo.src , 'logo')
+        activeClasses.map((className) => {
+            links[0].classList.add(className);
+        });
+    });
 
     function handleClick(e) {
         const links = document.querySelectorAll('nav#header-links a');
         links.forEach((link) => {
             console.log(link.classList, 'link');
-            link.classList.remove('text-blue-500');
+            activeClasses.map((className) => {
+                link.classList.remove(className);
+            });
         });
-        e.target.classList.add('active', 'text-blue-500');
+        activeClasses.map((className) => {
+            e.target.classList.add(className);
+        });
     }
 </script>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,5 +1,16 @@
-<section id="home" class="min-h-screen">
-
+<section id="home" class="min-h-screen w-full flex flex-col items-center justify-center p-20">
+    <h2 class="text-3xl font-bold">Hey there 👋</h2>
+    <h1 class="text-6xl font-bold">I'm Brian Monsales</h1>
+    <h2 class="text-4xl font-bold mt-3 bg-gradient-to-r from-fuchsia-600 to-indigo-600 bg-clip-text text-transparent text-shine">FullStack Web Developer</h2>
+    <div class="flex flex-row flex-wrap w-full h-full max-w-7xl min-h-[400px] mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="w-w-full sm:w-2/3 p-2">
+            
+        
+        </div>
+        <div class="w-w-full sm:w-1/3 p-2">
+        
+        </div>
+    </div>
 </section>
 
 <style>
