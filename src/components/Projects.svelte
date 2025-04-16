e0b4662e0fadd7e5b25937ec2659cfa6a472c081
Brian Monsales <brianmonsales@gmail.com>
2025-04-16 21:10:21 +0800

    partial update for projects component/section

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,7 +1,49 @@
 <section id="projects" class="w-full min-h-screen flex flex-col relative justify-center items-center py-25 overflow-hidden">
-    <h2 class="text-4xl">Projects</h2>
+    <div class="flex flex-col w-full h-full max-w-7xl min-h-[400px] mx-auto">
+        <h2 class="text-4xl">Projects</h2>
+        <div class="flex flex-row flex-wrap -m-2">
+            {#each projects as project}
+                <div class="w-full sm:w-1/2 lg:w-1/3 p-2 flex justify-center">
+                    <ProjectCard project={project} />
+                </div>
+            {/each}
+        </div>
+    </div>
 </section>
 
-<style>
-
-</style>
+<script>
+    import ProjectCard from '@components/ui/ProjectCard.svelte';
+    const projects = [
+        {
+            name: "Dating X",
+            imgSrc: '/projects/dating1.png',
+            company: "Optimail Co. Ltd. / Moebius Development Co. Ltd.",
+            date: "2018-2024",
+            description: "A multi domain dating app with theming built in to, it has messaging inside as well as push notifications for people to get notified whenever they got new messages or have been labeled as favorites for other users.",
+            url: "https://preview.datingdev.app/",
+            hasLiveDemo: true,
+            role: "Made the new version of the app from laravel and angularjs v2 to laravel 5 and vuejs v2, added the theming feature with total of 3 completed themes integrated within the app. Also added the push notification feature for the app. Overall responsible for the front end development of the app.From adding new features to fixing bugs. Considered myself the main contributer and maintainer of the app.",
+        },
+        {
+            name: "Skill Shift",
+            imgSrc: '/projects/skillshift.png',
+            company: "Freemight / Monstarlab Cebu",
+            date: "2018",
+            description: "A japanese job posting board that allows part time worksers to find jobs throughout Japan. You can filter out which prefecture / region you want to apply and what type of job you want.",
+            url: "https://www.skill-shift.com/",
+            hasLiveDemo: true,
+            role: "Started the project from scratch, created the whole front end of the app using laravel 5 and vuejs v2. Integrated the backend api with the frontend. Added features like filtering jobs by region and type of job. I had one collegue who eventually helped me to finish the project.",
+            
+        },
+        {
+            name: "Mee2box",
+            imgSrc: '/projects/mee2box1.png',
+            company: "Freemight / Monstarlab Cebu",
+            date: "2017",
+            description: "A japanese zoom-like video conferencing web app that allows you to create a room and invite clients to join, it allows file sharing inside the meeting as well so that you could brainstorm efficiently.",
+            url: "https://svenpham.webflow.io/project/mee2box",
+            hasLiveDemo: false,
+            role: "Started the project from scratch, made it using laravel 5 and vuejs v1 with Keen-UI (similar to Vuetify which is based in Material Design). Responsible in integrating authentication, video conferencing (used peerjs then eventually switched to SkywayJs), file uploads, and the user management. Eventually passed to a different development company after I made the initial version of the app. I was the only one who worked on the project."
+        }
+    ];
+</script>
---
+++ src/components/ui/ProjectCard.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { cn } from "@lib/utils";
+    let props = $props();
+    const project = props.project
+</script>
+
+<div class={cn('rounded-2xl relative aspect-[4/3] overflow-hidden group', props.class)}>
+    <img
+      src="{project.imgSrc}"
+      placeholder="blur"
+      alt="don corleone"
+      class="w-full object-cover m-0"
+    />
+    <!--  overlay  -->
+    <div
+      class="absolute inset-0 from-black/95 via-black/70 to-black/10 bg-gradient-to-t"
+    ></div>
+    <div class="text-gray-100 sm:px-4 py-0 sm:pb-3 text-left absolute pb-1 px-4 md:pb-4 inset-x-0 bottom-2 flex flex-col justify-end size-full">
+        <h3 class="text-lg font-bold tracking-tighter mt-3 mb-1">
+        {project.name}
+        </h3>
+        <p class="text-sm leading-5">{project.description}</p>
+    </div>
+</div>
